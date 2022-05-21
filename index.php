ec5ff16c0ca87aff2b5ef7f55e031c2d9bb79123
Garrett Morgan <gar.mor.pro@outlook.com>
2022-05-21 09:32:18 -0600

    update

--- index.php
+++ index.php
@@ -19,11 +19,46 @@
 
     <h2>Another day</h2>
 
-    <form action="fileUploadScript.php" method="post" enctype="multipart/form-data">
-        Upload a File:
-        <input type="file" name="the_file" id="fileToUpload">
-        <input type="submit" name="submit" value="Start Upload">
+    
+    
+    <form method="POST" action="index.php" enctype="multipart/form-data" >
+      <label  class="form-label">Name</label>
+      <input type="text" class="form-control" id="exampleInputEmail1" name="name" required > <br><br>
+      file : <input type="file" name="file"><br><br>
+      <br/>
+      <input type="submit" value="submit"  name="submit">
     </form>
 
 </body>
 </html>
+
+<?php
+$server = 'localhost';
+$user = "root";
+$password = "Morgan22!";
+$dbname = "storage";
+$conn = mysqli_connect($server, $user, $password, $dbname) or die("could not connect to the database");
+
+if(isset($_POST['submit'])){
+    $fileName = $_POST['name'];
+    $name = $_FILES['file']['name'];
+    $targetDir = "uploads/";
+    $targetFile = $targetDir.basename($_FILES['file']['name']);
+    $fileType = strtolower(pathinfo($targetFile,PATHINFO_EXTENSION));
+    $extensions_arr= array("jpg","jpeg","png");
+    if( in_array($fileType,$extensions_arr) ){
+    // Insert record
+    $query = " INSERT into `files` (`filename`) values('$name')";
+    mysqli_query($conn,$query);
+    // Upload file
+    move_uploaded_file($_FILES['file']['tmp_name'],$targetDir.$name);
+    } else echo " wrong file type ";
+
+    }
+    $sql = "SELECT  `filename` FROM `files` WHERE `filename`= '$name'";
+    $result = mysqli_query($conn,$sql);
+    $row = mysqli_fetch_array($result);
+    $image = $row['filename'];
+    $image_src = "uploads/".$image;
+ ?>
+<img src='<?php echo $image_src; ?>' >
