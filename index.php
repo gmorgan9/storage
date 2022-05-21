7f96b9150ad64da759eac3390f53963389f745da
Garrett Morgan <gar.mor.pro@outlook.com>
2022-05-20 22:34:17 -0600

    m

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Custom Styles -->
-    <link rel="stylesheet" type="text/css" href="assets/css/styles.css?v=1.6">
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css?v=<?php echo time() ?>">
 
 
     <title>Document</title>
@@ -17,9 +17,11 @@
             <h1 class="logo-header">MorganStorage</h1>
             <div class="nav-list">
                 <ul>
-                    <li id="item-one" class="nav-list-item"><a class="nav-link" href="/">link 1</a></li>
-                    <li id="item-two" class="nav-list-item"><a class="nav-link" href="login.php">link 2</a></li>
-                    <li id="item-three" class="nav-list-item"><a class="nav-link" href="register.php">link 3</a></li>
+                    <li id="item-one" class="nav-list-item"><a class="nav-link" href="/">Home</a></li>
+                <div class="logreg">
+                    <li id="item-two" class="nav-list-item"><a class="nav-link" href="login.php">Login</a></li>
+                    <li id="item-three" class="nav-list-item"><a class="nav-link" href="register.php">Register</a></li>
+                </div>
                 </ul>
             </div>
         </nav>
