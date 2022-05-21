ee93a221aa0a074fbfe0ea304266c05ed9998aa9
Garrett Morgan <gar.mor.pro@outlook.com>
2022-05-21 10:09:00 -0600

    updat

--- app/database/connect.php
+++ app/database/connect.php
@@ -8,7 +8,7 @@ $db_name = 'storage';
 $conn = new MySQLi($host, $user, $pass, $db_name);
 
 if ($conn) {
-    echo "Congrats! Connection Successful!";
+    // echo "Congrats! Connection Successful!";
 } else {
     die('Database connection error: ' . $conn->connect_error);
 }
