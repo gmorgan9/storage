531ab0e1498390e50671a234071a339882ee7803
Garrett Morgan <gar.mor.pro@outlook.com>
2022-05-21 20:32:18 -0600

    update

--- index.php
+++ index.php
@@ -22,11 +22,38 @@ session_start();
     <title>MorganStorage</title>
 </head>
 <body>
-    
-    <?php include(ROOT_PATH . "/app/includes/header.php") ?>
-    <?php include(ROOT_PATH . "/app/includes/main-heading.php") ?>
+	<div class="header">
+		<h2>Home Page</h2>
+	</div>
+	<div class="content">
+		<!-- notification message -->
+		<?php if (isset($_SESSION['success'])) : ?>
+			<div class="error success" >
+				<h3>
+					<?php 
+						echo $_SESSION['success']; 
+						unset($_SESSION['success']);
+					?>
+				</h3>
+			</div>
+		<?php endif ?>
+		<!-- logged in user information -->
+		<div class="profile_info">
+			<img src="images/user_profile.png"  >
 
-    <h2>Another day</h2>
+			<div>
+				<?php  if (isset($_SESSION['user'])) : ?>
+					<strong><?php echo $_SESSION['user']['username']; ?></strong>
 
+					<small>
+						<i  style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i> 
+						<br>
+						<a href="index.php?logout='1'" style="color: red;">logout</a>
+					</small>
+
+				<?php endif ?>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
