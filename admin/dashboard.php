2660434fccfca31a3fec22f143c8e08341e0cf09
Garrett Morgan <gar.mor.pro@outlook.com>
2022-05-21 22:44:14 -0600

    m

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -22,7 +22,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Custom Styles -->
-    <link rel="stylesheet" type="text/css" href="assets/css/styles.css?v=<?php echo time() ?>">
+    <link rel="stylesheet" type="text/css" href="../assets/css/styles.css?v=<?php echo time() ?>">
 
     <!-- Font Awesome -->
   	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
