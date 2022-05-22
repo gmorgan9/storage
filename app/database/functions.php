c9e7c29c11983f3ff318b515ffd160ab1ac6095a
Garrett Morgan <gar.mor.pro@outlook.com>
2022-05-21 22:50:03 -0600

    m

--- app/database/functions.php
+++ app/database/functions.php
@@ -50,7 +50,7 @@ function register(){
 					  VALUES('$username', '$email', '$user_type', '$password')";
 			mysqli_query($db, $query);
 			$_SESSION['success']  = "New user successfully created!!";
-			header('location: ../admin/dashboard.php');
+			header('location: /');
 		}else{
 			$query = "INSERT INTO users (username, email, user_type, password) 
 					  VALUES('$username', '$email', 'user', '$password')";
@@ -61,7 +61,7 @@ function register(){
 
 			$_SESSION['user'] = getUserById($logged_in_user_id); // put logged in user in session
 			$_SESSION['success']  = "You are now logged in";
-			header('location: /');				
+			header('location: admin/dashboard.php');				
 		}
 	}
 }
