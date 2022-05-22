cabb455e66c2355ed2ab7f93c39ff91e5aff2a9a
Garrett Morgan <gar.mor.pro@outlook.com>
2022-05-21 22:53:33 -0600

    m

--- app/database/functions.php
+++ app/database/functions.php
@@ -61,7 +61,7 @@ function register(){
 
 			$_SESSION['user'] = getUserById($logged_in_user_id); // put logged in user in session
 			$_SESSION['success']  = "You are now logged in";
-			header('location: /');				
+			header('location: admin/dashboard.php');				
 		}
 	}
 }
