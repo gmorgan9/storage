eb37a108f9a3801888b4a59860506d7a92abead1
Garrett Morgan <gar.mor.pro@outlook.com>
2022-05-21 22:37:33 -0600

    m

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ session_start();
 					<small>
 						<i  style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i> 
 						<br>
-						<a href="index.php?logout='1'" style="color: red;">logout</a>
+						<button class="btn"><a href="index.php?logout='1'" style="color: red;">logout</a></button>
 					</small>
 
 				<?php endif ?>
