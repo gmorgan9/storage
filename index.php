2866b8dfa334525f6b8b1fecbb350599dd3b612c
Garrett Morgan <gar.mor.pro@outlook.com>
2022-05-21 20:11:00 -0600

    update

--- index.php
+++ index.php
@@ -11,6 +11,12 @@ include('app/database/functions.php');
     <!-- Custom Styles -->
     <link rel="stylesheet" type="text/css" href="assets/css/styles.css?v=<?php echo time() ?>">
 
+    <!-- Font Awesome -->
+  	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Candal|Lora" rel="stylesheet">
 
     <title>MorganStorage</title>
 </head>
